3ee6c7a425e67e354b3b884d968f5ceac53a236f
Your Name <fidelomolo7@gmail.com>
2019-06-23 22:10:27 +0300

    first commit

---
+++ app/src/main/java/com/example/nearbyhospitals/Bottom.java
@@ -0,0 +1,43 @@
+package com.example.nearbyhospitals;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Bottom extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_dashboard:
+
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/com/example/nearbyhospitals/NearbyPlaceModels/ModelClasses/PlaceNameModel.java
@@ -0,0 +1,19 @@
+package com.example.nearbyhospitals.NearbyPlaceModels.ModelClasses;
+
+public class PlaceNameModel {
+
+    String name;
+
+    public PlaceNameModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
